1fc277121871337c1a53c7ea543246dd33682628
Daniele Vinci <vinci.dani@gmail.com>
2019-07-06 21:36:55 -0400

    code review

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
-const saltRounds = 10;
-const salt = bcrypt.genSaltSync(saltRounds);
+//const saltRounds = 10;
+//const salt = bcrypt.genSaltSync(saltRounds);
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -26,128 +31,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// const database = {
-//   users : [
-//     {
-//       id :'123',
-//       name : 'John',
-//       email : 'john@gmail.com',
-//       password : 'cookies',
-//       entries : 0,
-//       joined : new Date()
-//     },
-//     {
-//       id :'124',
-//       name : 'Sally',
-//       email : 'sally@gmail.com',
-//       password : 'bananas',
-//       entries : 0,
-//       joined : new Date()
-//     }
-//   ]
-// }
 
-app.get('/', (req, res) => {
-  res.send(database.users);
-})
-
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where( 'email', '=', req.body.email  )
-    .then(data=> {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user=> {
-            res.json(user[0])
-          })
-          .catch(err=>res.status(400).json('Unable to get user'))
-      } else {
-        res.status(400).json('Wrong credentials')
-      }
-    })
-    .catch(err=> res.status(400).json('Wrong credentials'))
-  // if (req.body.email === database.users[0].email &&
-  //     req.body.password === database.users[0].password) {
-  //       res.json(database.users[0]);
-  //     } else {
-  //       res.status(400).json('Error logging in');
-  //     }
-})
-
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password, salt);
-  // const hash = bcrypt.hash(password, saltRounds, function(err, hash) {});
-  db.transaction(trx => {
-    trx.insert({
-      hash : hash,
-      email : email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail=> {
-      return trx('users')
-      .returning('*')
-      .insert({
-        email: loginEmail[0],
-        name : name,
-        joined : new Date()
-      })
-      .then(user => {
-        res.json(user[0]);
-      })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err=>res.status(400).json('Unable to register...'))
-})
-
-app.get('/profile/:id', (req, res) => {
-  // console.log(req.params);
-  const { id } = req.params;
-  db
-    .select('*')
-    .from('users')
-    .where({
-      id: id
-    })
-    .then(user=>{
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found!')
-      }
-    })
-    .catch(err=>res.status(400).json('Error getting user!'))
-})
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err=>res.status(400).json('Unable to get entries'))
-
-  // let found = false;
-  // database.users.forEach(user => {
-  //   if (user.id === id) {
-  //     found = true;
-  //     user.entries ++;
-  //     return res.json(user.entries);
-  //   }
-  // })
-  // if (!found) {
-  //   res.status(400).json('Not found!');
-  // }
-})
-
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, ()=>{
   console.log('App is running on port 3000');
@@ -182,3 +71,27 @@ and checks the other users to see who have submitted the most and give them a ra
 (update on the user profile of the score) and this will return the updated user obj
 
 */
+
+
+
+
+// const database = {
+//   users : [
+//     {
+//       id :'123',
+//       name : 'John',
+//       email : 'john@gmail.com',
+//       password : 'cookies',
+//       entries : 0,
+//       joined : new Date()
+//     },
+//     {
+//       id :'124',
+//       name : 'Sally',
+//       email : 'sally@gmail.com',
+//       password : 'bananas',
+//       entries : 0,
+//       joined : new Date()
+//     }
+//   ]
+// }
